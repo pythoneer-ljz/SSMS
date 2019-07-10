668e28ab991c520b708b834fa90041afcc656861
Your Name <email@example.com>
2019-07-10 18:51:02 +0800

    frist commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace SQL_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtSQL = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtEntityClass = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbTable = new System.Windows.Forms.ComboBox();
+            this.cmbDatabase = new System.Windows.Forms.ComboBox();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtUserID = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.执行F5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.清空ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtSQL);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(746, 399);
+            this.splitContainer1.SplitterDistance = 183;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // txtSQL
+            // 
+            this.txtSQL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSQL.Location = new System.Drawing.Point(0, 0);
+            this.txtSQL.Multiline = true;
+            this.txtSQL.Name = "txtSQL";
+            this.txtSQL.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtSQL.Size = new System.Drawing.Size(746, 183);
+            this.txtSQL.TabIndex = 0;
+            this.txtSQL.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSQL_KeyDown);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(746, 212);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 119);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(760, 431);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.splitContainer1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(752, 405);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "SQL查询";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtEntityClass);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(752, 405);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "实体类";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtEntityClass
+            // 
+            this.txtEntityClass.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtEntityClass.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtEntityClass.Location = new System.Drawing.Point(3, 3);
+            this.txtEntityClass.Multiline = true;
+            this.txtEntityClass.Name = "txtEntityClass";
+            this.txtEntityClass.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtEntityClass.Size = new System.Drawing.Size(746, 399);
+            this.txtEntityClass.TabIndex = 1;
+            this.txtEntityClass.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtEntityClass_KeyDown);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cmbTable);
+            this.groupBox1.Controls.Add(this.cmbDatabase);
+            this.groupBox1.Controls.Add(this.btnQuery);
+            this.groupBox1.Controls.Add(this.btnLogin);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtServer);
+            this.groupBox1.Controls.Add(this.txtUserID);
+            this.groupBox1.Location = new System.Drawing.Point(12, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(760, 61);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "连接信息";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(535, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "表";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(372, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "库";
+            // 
+            // cmbTable
+            // 
+            this.cmbTable.FormattingEnabled = true;
+            this.cmbTable.Location = new System.Drawing.Point(558, 26);
+            this.cmbTable.Name = "cmbTable";
+            this.cmbTable.Size = new System.Drawing.Size(121, 20);
+            this.cmbTable.TabIndex = 3;
+            this.cmbTable.TextChanged += new System.EventHandler(this.cmbTable_TextChanged);
+            // 
+            // cmbDatabase
+            // 
+            this.cmbDatabase.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDatabase.FormattingEnabled = true;
+            this.cmbDatabase.Location = new System.Drawing.Point(395, 25);
+            this.cmbDatabase.Name = "cmbDatabase";
+            this.cmbDatabase.Size = new System.Drawing.Size(121, 20);
+            this.cmbDatabase.TabIndex = 3;
+            this.cmbDatabase.TextChanged += new System.EventHandler(this.cmbDatabase_TextChanged);
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(685, 24);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(59, 23);
+            this.btnQuery.TabIndex = 2;
+            this.btnQuery.Text = "查询";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(275, 22);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(59, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "登录";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(199, 24);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(70, 21);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(17, 24);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(100, 21);
+            this.txtServer.TabIndex = 1;
+            this.txtServer.Text = "(local)";
+            // 
+            // txtUserID
+            // 
+            this.txtUserID.Location = new System.Drawing.Point(123, 24);
+            this.txtUserID.Name = "txtUserID";
+            this.txtUserID.Size = new System.Drawing.Size(70, 21);
+            this.txtUserID.TabIndex = 1;
+            this.txtUserID.Text = "sa";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.执行F5ToolStripMenuItem,
+            this.清空ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 25);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 执行F5ToolStripMenuItem
+            // 
+            this.执行F5ToolStripMenuItem.Name = "执行F5ToolStripMenuItem";
+            this.执行F5ToolStripMenuItem.ShortcutKeyDisplayString = "";
+            this.执行F5ToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F5;
+            this.执行F5ToolStripMenuItem.Size = new System.Drawing.Size(65, 21);
+            this.执行F5ToolStripMenuItem.Text = "执行(F5)";
+            this.执行F5ToolStripMenuItem.Click += new System.EventHandler(this.执行F5ToolStripMenuItem_Click);
+            // 
+            // 清空ToolStripMenuItem
+            // 
+            this.清空ToolStripMenuItem.Name = "清空ToolStripMenuItem";
+            this.清空ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.清空ToolStripMenuItem.Text = "清空";
+            this.清空ToolStripMenuItem.Click += new System.EventHandler(this.清空ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "SQL_TOOL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtSQL;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUserID;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.ComboBox cmbDatabase;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.ComboBox cmbTable;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEntityClass;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 执行F5ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 清空ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SQL_Tool
+{
+    public partial class Form1 : Form
+    {
+
+        public string tableName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSQL_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+
+                //Ctrl+A全选
+                if (e.Modifiers == Keys.Control && e.KeyCode == Keys.A)
+                {
+                    ((TextBox)sender).SelectAll();
+                }
+
+                
+
+
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string server = txtServer.Text, userID = txtUserID.Text, password = txtPassword.Text;
+            DBHelper.connStr = "Data Source=" + server + ";Initial Catalog=;Persist Security Info=True;User ID=" + userID + ";Password=" + password + ";Connect Timeout=1";
+            DataTable dt = DBHelper.GetDataTable("select name from sysdatabases");
+            cmbDatabase.ValueMember = "name";
+            cmbDatabase.DisplayMember = "name";
+            cmbDatabase.DataSource = dt;
+        }
+
+        private void cmbDatabase_TextChanged(object sender, EventArgs e)
+        {
+            string database = cmbDatabase.Text;
+            string server = txtServer.Text, userID = txtUserID.Text, password = txtPassword.Text;
+            DBHelper.connStr = "Data Source=" + server + ";Initial Catalog=" + database + ";Persist Security Info=True;User ID=" + userID + ";Password=" + password + ";Connect Timeout=1";
+
+            
+            DataTable dt = DBHelper.GetDataTable("select name from sysobjects where xtype = 'U'");
+            if (dt.Rows.Count > 0)
+            {
+                cmbTable.ValueMember = "name";
+                cmbTable.DisplayMember = "name";
+                cmbTable.DataSource = dt;
+            }
+            else {
+                cmbTable.DataSource = null;
+            }
+        }
+
+        private void cmbTable_TextChanged(object sender, EventArgs e)
+        {
+            tableName = cmbTable.Text;
+
+            //加载实体类
+            string sql = "select column_name,data_type from " + cmbDatabase.Text + ".information_schema.columns where table_name='" + tableName + "'";
+            DataTable dt = DBHelper.GetDataTable(sql);
+
+            StringBuilder sb = new StringBuilder("public class " + tableName + "\r\n{");
+
+            foreach (DataRow field in dt.Rows)
+            {
+                string column_name = field["column_name"].ToString();
+                string data_type = DBTpyeConvertCSType(field["data_type"].ToString());
+                sb.Append("\r\n\tprivate "+ data_type + " _" + column_name + ";\r\n\tpublic " + data_type + " " + column_name + "\r\n\t{\r\n\t\tget { return _" + column_name + "; }\r\n\t\tset { _" + column_name + " = value; }\r\n\t}\r\n");
+            }
+
+            sb.Append("}");
+
+            txtEntityClass.Text = sb.ToString();
+
+        //private int eID;
+        //public int EID
+        //{
+        //    get { return eID; }
+        //    set { eID = value; }
+        //}
+
+
+    }
+
+
+        private string DBTpyeConvertCSType(string dbtype)
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("SQL", "C#");
+            hashtable.Add("bigint", "long");
+            hashtable.Add("binary", "object");
+            hashtable.Add("bit", "bool");
+            hashtable.Add("char", "string");
+            hashtable.Add("datetime", "DateTime");
+            hashtable.Add("decimal", "decimal");
+            hashtable.Add("float", "double");
+            hashtable.Add("image", "byte[]");
+            hashtable.Add("int", "int");
+            hashtable.Add("money", "decimal");
+            hashtable.Add("nchar", "string");
+            hashtable.Add("ntext", "string");
+            hashtable.Add("numeric", "decimal");
+            hashtable.Add("nvarchar", "string");
+            hashtable.Add("real", "float");
+            hashtable.Add("smalldatetime", "DateTime");
+            hashtable.Add("smallint", "short");
+            hashtable.Add("smallmoney", "decimal");
+            hashtable.Add("text", "string");
+            hashtable.Add("timestamp", "byte[]");
+            hashtable.Add("tinyint", "byte");
+            hashtable.Add("uniqueidentifier", "Guid");
+            hashtable.Add("varbinary", "byte[]");
+            hashtable.Add("varchar", "string");
+            hashtable.Add("xml", "string");
+            hashtable.Add("sql_variant", "object");
+            string text = "未知类型";
+            if (!string.IsNullOrEmpty(dbtype))
+            {
+                try
+                {
+                    text = hashtable[dbtype].ToString();
+                    if (string.IsNullOrEmpty(text))
+                    {
+                        text = "未知类型";
+                    }
+                }
+                catch
+                {
+                    text = "未知类型";
+                }
+            }
+            return text;
+        }
+
+        private void txtEntityClass_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Ctrl+A全选
+            if (e.Modifiers == Keys.Control && e.KeyCode == Keys.A)
+            {
+                ((TextBox)sender).SelectAll();
+            }
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            tableName = cmbTable.Text;
+
+            txtSQL.Text += "\r\nSELECT * FROM " + tableName + "";
+        }
+
+        private void 执行F5ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                    //if (!txtSQL.Focused) return;
+                    if (txtSQL.SelectionLength > 0)
+                    {
+                        dataGridView1.DataSource = DBHelper.GetDataTable(txtSQL.SelectedText);
+                    }
+                    else
+                    {
+                        dataGridView1.DataSource = DBHelper.GetDataTable(txtSQL.Text);
+                    }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void 清空ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("是否清空?","清空",MessageBoxButtons.YesNo)==DialogResult.Yes)
+            txtSQL.Text = "";
+        }
+    }
+}
